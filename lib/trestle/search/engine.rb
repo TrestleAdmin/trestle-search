6d47a2e34f01726eef263aca576f7bf53bdf7cf1
Sam Pohlenz <sam@sampohlenz.com>
2016-09-12 13:31:45 +0930

    Revert initialization changes

--- lib/trestle/search/engine.rb
+++ lib/trestle/search/engine.rb
@@ -3,7 +3,7 @@ module Trestle
     class Engine < ::Rails::Engine
       config.assets.precompile << "trestle/search.css" << "trestle/search.js"
 
-      config.before_initialize do
+      initializer :extensions do
         Trestle::Resource.extend(Trestle::Search::Resource)
         Trestle::Resource::Builder.send(:include, Trestle::Search::Builder)
         Trestle::Resource::Controller.send(:prepend, Trestle::Search::Controller)
