130a89cb4affb83baddd578ade909fb27345749a
Sam Pohlenz <sam@sampohlenz.com>
2018-08-05 16:32:27 +0930

    Move search box to resource index header

--- config/initializers/trestle.rb
+++ config/initializers/trestle.rb
@@ -1,11 +1,11 @@
 Trestle.configure do |config|
   config.persistent_params << :q
-  
+
   config.hook("stylesheets") do
     stylesheet_link_tag "trestle/search"
   end
 
-  config.hook("view.header") do
+  config.hook("resource.index.header", if: -> { admin.searchable? }) do
     render "trestle/search/search"
   end
 end
