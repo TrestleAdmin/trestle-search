10b72c780218d253a2fba57b5c8ea0774fa7176f
Sam Pohlenz <sam@sampohlenz.com>
2020-05-04 19:16:19 +0930

    Add search results breadcrumb before rendering index action

--- lib/trestle/search/controller.rb
+++ lib/trestle/search/controller.rb
@@ -2,11 +2,11 @@ module Trestle
   module Search
     module Controller
       def index
-        super
-
         if admin.searchable? && params[:q].present?
           breadcrumb t("admin.search.results", default: "Search Results"), { q: params[:q] }
         end
+
+        super
       end
     end
   end
