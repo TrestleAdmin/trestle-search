b961981714aecbd8d29829e3764e2d4fe35c9471
Sam Pohlenz <sam@sampohlenz.com>
2024-05-27 19:33:42 +0930

    Replace Travis config with Github Actions workflow

--- spec/dummy/config/application.rb
+++ spec/dummy/config/application.rb
@@ -17,14 +17,7 @@ Bundler.require(*Rails.groups)
 module Dummy
   class Application < Rails::Application
     # Initialize configuration defaults for current Rails version.
-    case Rails.version.split(".").first(2).join(".")
-    when '6.0'
-      config.load_defaults 6.0
-    when '5.2'
-      config.load_defaults 5.2
-    when '5.1'
-      config.load_defaults 5.1
-    end
+    config.load_defaults Rails::VERSION::STRING.to_f
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
